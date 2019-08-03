09de0dda3089690ba45846f56e73f9bbedd5fa68
Emily Tucker <emilytucker4@yahoo.com>
2019-08-03 00:54:54 -0400

    added if statments

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -12,4 +12,26 @@ document.onkeyup = function(event){
     var computerGuess = computerChoices [Math.floor(Math.random() * computerChoices.length)];
 
     var choices = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "g", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z", "0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
+
+    if (choices.indexOf(playerGuess) > -1){
+        
+        if (playerGuess === computerGuess){
+            wins++;
+            guessesMade = 9;
+            playersChoices =[];
+        }
+
+        if (playerGuess != computerGuess){
+            guessesMade --;
+            playersChoices.push(playerGuess);
+        }
+
+        if (guessesMade === 0){
+            guessesMade = 9;
+            losses ++;
+            playersChoices[];
+        }
+
+        
+    }
 }
