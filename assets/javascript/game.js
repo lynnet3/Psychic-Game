73561aae79959eebfd7249151e0183eecfd70583
Emily Tucker <emilytucker4@yahoo.com>
2019-08-02 23:31:47 -0400

    finished adding the array for the computer choices

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,1 +1,3 @@
-var computerChoices = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "g", "k", "l", "m",nopqrstuvwxyz0123456789]
+var computerChoices = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "g", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z", "0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
+
+    
