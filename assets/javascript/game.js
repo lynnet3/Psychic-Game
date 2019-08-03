5de5291f5ee387180b056b1dcf00f11b2ee91ab9
Emily Tucker <emilytucker4@yahoo.com>
2019-08-03 00:19:34 -0400

    added the variables for wins losses and choices in the javascript

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,3 +1,6 @@
 var computerChoices = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "g", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z", "0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
 
-    
+var wins = 0;
+var losses = 0;
+var guessesMade = 9;
+var playersChoices = [];
