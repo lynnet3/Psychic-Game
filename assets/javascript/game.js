a82ab762bf3091bc975246f48a0105762a78f2b4
Emily Tucker <emilytucker4@yahoo.com>
2019-08-03 22:53:23 -0400

    took the math.floor equation out of the keyupvfunction

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,18 +1,19 @@
 var computerChoices = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "g", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z", "0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
-
 var wins = 0;
 var losses = 0;
 var guessesMade = 9;
 var playersChoices = [];
 
+
+var computerGuess = computerChoices [Math.floor(Math.random() * computerChoices.length)];
+
 document.onkeyup = function(event){
     
     var playerGuess = event.key;
     
-    var computerGuess = computerChoices [Math.floor(Math.random() * computerChoices.length)];
+    
 
-
-    if (choices.indexOf(playerGuess) > -1){
+    if (computerChoices.indexOf(playerGuess) > -1){
         
         if (playerGuess === computerGuess){
             wins++;
